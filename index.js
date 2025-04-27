452a41913dd88d6b1226d278a25086cd8ea35bb7
MietVdh <miet.vanderheyden@gmail.com>
2025-04-27 11:14:49 -0400

    Completed required functionality

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
-var app = express();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const file = req.file;
+  const name = file.originalname;
+  const type = file.mimetype;
+  const size = file.size;
+  res.json({ name, type, size });
 });
 
 
